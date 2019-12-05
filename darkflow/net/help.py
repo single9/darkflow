71e5238ab81b2a475151d9adb6bebb3bdcebaf1b
Duye Chen <k078264@gmail.com>
2019-12-05 17:09:39 +0800

    Migration to tensorflow 1.15

--- darkflow/net/help.py
+++ darkflow/net/help.py
@@ -45,7 +45,7 @@ def load_old_graph(self, ckpt):
     ckpt_loader = create_loader(ckpt)
     self.say(old_graph_msg.format(ckpt))
     
-    for var in tf.global_variables():
+    for var in tf.compat.v1.global_variables():
         name = var.name.split(':')[0]
         args = [name, var.get_shape()]
         val = ckpt_loader(args)
@@ -156,7 +156,7 @@ def to_darknet(self):
     darknet_ckpt = self.darknet
 
     with self.graph.as_default() as g:
-        for var in tf.global_variables():
+        for var in tf.compat.v1.global_variables():
             name = var.name.split(':')[0]
             var_name = name.split('-')
             l_idx = int(var_name[0])
